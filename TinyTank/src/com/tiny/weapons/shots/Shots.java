514d1d77aa3612556a91ad900c45f78652f3b54a
Zylox <zch5of6@earthlink.net>
2013-10-18 12:37:01 -0500

    working on getting it up and running

--- TinyTank/src/com/tiny/weapons/shots/Shots.java
+++ TinyTank/src/com/tiny/weapons/shots/Shots.java
@@ -1,5 +1,16 @@
 package com.tiny.weapons.shots;
 
+import org.newdawn.slick.geom.Vector2f;
+
+import com.tiny.weapons.Shot;
+
 public enum Shots {
 
+	NORMAL_SHOT(new NormalShot(new Vector2f(0,0), 10, 1, null)),
+	BIG_SHOT(new NormalShot(new Vector2f(0,0), 30, 1, null));
+	
+	Shots(Shot shot){
+		
+	}
+	
 }
