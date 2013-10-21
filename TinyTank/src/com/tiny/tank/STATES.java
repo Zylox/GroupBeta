60d06cee21a7750a632422f1da06079d2f38761e
Ben Stein <stein321@gmail.com>
2013-10-21 12:20:06 -0500

    Project opens to Main Menu. Button is there, but clicking in the wrong place starts the game right now

--- TinyTank/src/com/tiny/tank/STATES.java
+++ TinyTank/src/com/tiny/tank/STATES.java
@@ -4,9 +4,9 @@ import org.newdawn.slick.state.BasicGameState;
 
 public enum STATES {
 	MAIN_MENU(new Main_Menu(1)),
-	SELECT_WEAPONS_MENU(new Select_Weapons_Menu(1)), 
-	MAIN_GAMEPLAY(new Main_Gameplay(2)),
-	PAUSE_MENU(new Menu(3));
+	SELECT_WEAPONS_MENU(new Select_Weapons_Menu(2)), 
+	MAIN_GAMEPLAY(new Main_Gameplay(3)),
+	PAUSE_MENU(new Menu(4));
 	
 	private int id;
 	private BasicGameState state;
