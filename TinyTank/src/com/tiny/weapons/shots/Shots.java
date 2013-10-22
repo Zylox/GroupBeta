461993efac75374b79fd911de3546146e40b1c46
Zylox <zch5of6@earthlink.net>
2013-10-21 19:44:03 -0500

    fixed size of shot

--- TinyTank/src/com/tiny/weapons/shots/Shots.java
+++ TinyTank/src/com/tiny/weapons/shots/Shots.java
@@ -8,7 +8,7 @@ public enum Shots {
 
 	NORMAL_SHOT(new NormalShot(new Vector2f(0,0), 10, 1, null, "normal shot")),
 	BIG_SHOT(new NormalShot(new Vector2f(0,0), 30, 1, null, "big shot")),
-	FILL_SHOT(new FillShot(new Vector2f(0,0), 1000,1,null,"fill shot"));
+	FILL_SHOT(new FillShot(new Vector2f(0,0), 30,1,null,"fill shot"));
 	
 	Shot shot;
 	
