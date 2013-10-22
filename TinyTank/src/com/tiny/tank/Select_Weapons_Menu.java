a6d1f2b30cecc46603f6a13642b004c49c347242
Zylox <zch5of6@earthlink.net>
2013-10-21 19:41:05 -0500

    hopefully this works

--- TinyTank/src/com/tiny/tank/Select_Weapons_Menu.java
+++ TinyTank/src/com/tiny/tank/Select_Weapons_Menu.java
@@ -3,45 +3,54 @@ package com.tiny.tank;
 import java.awt.TextField;
 
 import org.lwjgl.input.Mouse;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.Font;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-
 import com.tiny.weapons.shots.Shots;
 
-public class Select_Weapons_Menu extends BasicGameState {
+
+
+public class Select_Weapons_Menu extends BasicGameState  {
 	private int id;
-	Font font;
-	TextField textField;
+	 Font font;
+	  TextField textField;
+	 
 
 	private Image title = null;
-	private Image p1_title = null;
-	private Image p2_title = null;
-	private Image button_play = null;
-	private Image button_menu = null;
-	private Image ammo_title = null;
+	private Image p1_title=null;
+	private Image p2_title=null;
+	private Image button_play= null;
+	private Image button_menu= null;
+	private Image ammo_title= null;
 	public Shots[] shots = Shots.values();
 	private int total = shots.length;
 	Image background = null;
-
-	public Select_Weapons_Menu(int id) {
-		this.id = id;
-	}
-
-	public void init(GameContainer container, StateBasedGame game)
-			throws SlickException {
-
-		g.setColor(new Color(176, 176, 176, 0.8f));
-		g.fillRect(300, 200, 200, 300);
-
-	}
+	
+	
+	
+	
+	public Select_Weapons_Menu(int id){
+			this.id = id;
+		}
 		
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
+	
+	
+		public void init(GameContainer container, StateBasedGame game)
+		throws SlickException {
+			
+
+			background = new Image("res/bg.jpg");
+			title = new Image("res/Weapon_Select_ title.png");
+			button_play= new Image("res/play_button.png");
+			button_menu= new Image("res/back_button.png");
+			p1_title= new Image("res/P1.png");
+			p2_title= new Image("res/P2.png");
+			ammo_title= new Image("res/ammo.png");
+		
+		}
+			
+
+		public void render(GameContainer container, StateBasedGame game, Graphics g)
 		throws SlickException {
 			
 			background.draw(0,0);
@@ -87,26 +96,27 @@ public class Select_Weapons_Menu extends BasicGameState {
 					
 		}
 
+		public void FillWeapons(GameContainer container, StateBasedGame game, Graphics g) throws SlickException
+		{
+			int x = 320;
+			int y = 200;
 
-	public void FillWeapons(GameContainer container, StateBasedGame game,
-			Graphics g) throws SlickException {
-		int x = 320;
-		int y = 200;
-
-		g.setColor(Color.black);
-		for (int i = 0; i < total; i++) {
-			String weapon = shots[i].toString();
-			g.drawString(weapon, x, y);
-			System.out.println(shots[i]);
-			y = y + 20;// next weapon will be placed below the previous
+			g.setColor(Color.black);
+			for(int i=0; i<total; i++)
+			{
+				String weapon = shots[i].toString();
+				g.drawString(weapon, x, y);	
+				System.out.println(shots[i]);
+				y=y+20;// next weapon will be placed below the previous
+			}
 		}
-
-	}
-
-	@Override
-	public int getID() {
-		// TODO Auto-generated method stub
+		public int getID() {
 		return id;
-	}
+		}
+		
+		
 
-}
+		
+		
+		
+	}
