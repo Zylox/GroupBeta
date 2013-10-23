6d875235ddef91cac2097f9d8bc16513d21c1b94
Ben Stein <stein321@gmail.com>
2013-10-22 19:44:26 -0500

    changed some Ids

--- TinyTank/src/com/tiny/tank/Select_Weapons_Menu.java
+++ TinyTank/src/com/tiny/tank/Select_Weapons_Menu.java
@@ -2,10 +2,8 @@ package com.tiny.tank;
 
 
 import org.lwjgl.input.Mouse;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
