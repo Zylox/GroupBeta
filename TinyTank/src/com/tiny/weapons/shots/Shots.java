7f3862ef1452a525125fa333c44ffccbf20e21b2
Zylox <zch5of6@earthlink.net>
2013-10-23 11:05:44 -0500

    changed some timings

--- TinyTank/src/com/tiny/weapons/shots/Shots.java
+++ TinyTank/src/com/tiny/weapons/shots/Shots.java
@@ -6,10 +6,10 @@ import com.tiny.weapons.Shot;
 
 public enum Shots {
 
-	NORMAL_SHOT(new NormalShot(new Vector2f(0,0), 10, 1, null,1,.1f, "normal shot")),
+	NORMAL_SHOT(new NormalShot(new Vector2f(0,0), 10, 1, null,1,.8f, "normal shot")),
 	BIG_SHOT(new NormalShot(new Vector2f(0,0), 30, 1, null,1,.8f, "big shot")),
 	//CRATER_SHOT(new CraterShot(new Vector2f(0,0), 30,1,null,"crater shot")), doesnt work yet
-	FILL_SHOT(new FillShot(new Vector2f(0,0), 30,1,null,1,.5f,"fill shot"));
+	FILL_SHOT(new FillShot(new Vector2f(0,0), 30,1,null,1,.8f,"fill shot"));
 	
 	Shot shot;
 	
