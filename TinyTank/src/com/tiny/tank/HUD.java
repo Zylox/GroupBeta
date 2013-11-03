e02c0cdc9915ef6b38fb255915aede6c3927f5db
adamlutz <adamlutz@mwc-082230.dhcp.missouri.edu>
2013-11-03 14:33:35 -0600

    Adding HUD class

---
+++ TinyTank/src/com/tiny/tank/HUD.java
@@ -0,0 +1,11 @@
+package com.tiny.tank;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+
+
+public class HUD {
+
+}
