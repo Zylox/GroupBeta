b88cd5ba29be6c5e445efcde36d0b38651ac34fc
Zylox <zch5of6@earthlink.net>
2013-11-20 12:07:40 -0600

    changed some crap BEN S broke again

--- TinyTank/src/com/tiny/weapons/shots/Shots.java
+++ TinyTank/src/com/tiny/weapons/shots/Shots.java
@@ -6,7 +6,7 @@ import com.tiny.weapons.Shot;
 
 public enum Shots {
 
-	NORMAL_SHOT(new NormalShot(new Vector2f(0,0), 25, 1, null,1,.2f,20, "normal shot")),
+	NORMAL_SHOT(new NormalShot(new Vector2f(0,0), 25, 1, null,1,.2f,20, "reg shot")),
 	BIG_SHOT(new NormalShot(new Vector2f(0,0), 40, 1, null,1,.8f,40, "big shot")),
 	//CRATER_SHOT(new CraterShot(new Vector2f(0,0), 30,1,null,"crater shot")), doesn't work yet
 	FILL_SHOT(new FillShot(new Vector2f(0,0), 30,1,null,1,.8f,0,"fill shot"));
